e6720bc093ef86e7f2e9971f5159fcaebee3e0b2
noyrada <noyradastudentiq@gmail.com>
2024-07-31 22:39:21 +0700

    first configure

---
+++ src/main/java/com/project/samplecrud_sb/SampleCrudSbApplication.java
@@ -0,0 +1,13 @@
+package com.project.samplecrud_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCrudSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleCrudSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/samplecrud_sb/SampleCrudSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.samplecrud_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCrudSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
